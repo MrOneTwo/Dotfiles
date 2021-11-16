469ac8f4fa147930e540c9f7882b8d01d127e137
MrOneTwo <ciesielskimm@gmail.com>
2021-11-16 15:03:53 +0100

    Improve fzy for fish

--- .config/fish/functions/fzy_key_bindings.fish
+++ .config/fish/functions/fzy_key_bindings.fish
@@ -3,8 +3,13 @@ function fzy_key_bindings
   function fzy-file-widget
     set -l selected_path (find * -type f | fzy)
 
-    commandline -i $selected_path
-    commandline -f repaint
+    if test -n "$selected_path"
+      commandline -i $selected_path
+      commandline -f repaint
+    else
+      # FIXME(michalc): this paints a new line when nothing gets selected
+      commandline -f repaint
+    end
   end
 
   bind \ct fzy-file-widget
